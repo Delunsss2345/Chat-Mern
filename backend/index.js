11375701b7cc9c02400412b9bc1d1f2e92a13f4d
Delunsss2345 <huypham1459@gmail.com>
2025-06-01 11:34:37 +0700

    up

--- backend/index.js
+++ backend/index.js
@@ -1,40 +1,42 @@
-import express from "express" ;
-import dotenv from "dotenv"
-import { connectDB } from "./src/lib/db.js";
-import authRouters from "./src/routers/auth.routers.js"
-import messageRouters from "./src/routers/message.routers.js"
+import express from "express";
+import dotenv from "dotenv";
 import cookieParser from "cookie-parser";
-import {app , server} from "./src/lib/socket.js"
-import cors from 'cors' ;
+import cors from "cors";
 
-import path from "path"
-dotenv.config()  ;
+import path from "path";
 
-const __dirname = path.resolve() ; 
-const port = process.env.PORT ; 
-app.use(express.json({ limit: "50mb" }));
-app.use(express.urlencoded({ limit: "50mb", extended: true }));
-app.use(cookieParser()) ; 
-app.use(cors({
-  origin: 'http://localhost:5173',  
-  credentials: true,               
-  optionsSuccessStatus: 200         
-}));
+import { connectDB } from "./lib/db.js";
 
+import authRoutes from "./routes/auth.route.js";
+import messageRoutes from "./routes/message.route.js";
+import { app, server } from "./lib/socket.js";
 
-app.use('/api/auth' , authRouters) ; 
-app.use('/api/messages' , messageRouters) ; 
+dotenv.config();
 
-if(process.env.NODE_ENV === "production") {
-  app.use(express.static(path.join(__dirname , "../frontend/dist")))
+const PORT = process.env.PORT;
+const __dirname = path.resolve();
 
-  app.get("*" , (req , res) => {
-    res.sendFile(path.join(__dirname , "../frontend" , "dist" , "index.html"))
+app.use(express.json());
+app.use(cookieParser());
+app.use(
+  cors({
+    origin: "http://localhost:5173",
+    credentials: true,
   })
+);
+
+app.use("/api/auth", authRoutes);
+app.use("/api/messages", messageRoutes);
+
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static(path.join(__dirname, "../frontend/dist")));
+
+  app.get("*", (req, res) => {
+    res.sendFile(path.join(__dirname, "../frontend", "dist", "index.html"));
+  });
 }
 
-server.listen(port , () => {
-    console.log(`Connect http://localhost:${port}`) ; 
-    connectDB() ; 
-}) ; 
-
+server.listen(PORT, () => {
+  console.log("server is running on PORT:" + PORT);
+  connectDB();
+});
