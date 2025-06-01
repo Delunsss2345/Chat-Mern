f2831224414a432c62091fec08da68ad1ae704d2
Delunsss2345 <huypham1459@gmail.com>
2025-06-01 11:20:55 +0700

    up

--- frontend/src/lib/axios.js
+++ frontend/src/lib/axios.js
@@ -1,7 +1,7 @@
 import axios from 'axios'
 
 const instance = axios.create({
-  baseURL: 'http://localhost:27011/api/',
+  baseURL: import.meta.env.MODE === 'development' ?  'http://localhost:27011/api/' : '/api' ,
   withCredentials:true 
 });
 
