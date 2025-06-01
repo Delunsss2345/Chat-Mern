abbea82805b718643a9621aa43f2ca1df241338d
Delunsss2345 <huypham1459@gmail.com>
2025-06-01 11:07:31 +0700

    up

--- backend/index.js
+++ backend/index.js
@@ -6,9 +6,11 @@ import messageRouters from "./src/routers/message.routers.js"
 import cookieParser from "cookie-parser";
 import {app , server} from "./src/lib/socket.js"
 import cors from 'cors' ;
+
+import path from "path"
 dotenv.config()  ;
 
-
+const __dirname = path.resolve() ; 
 const port = process.env.PORT ; 
 app.use(express.json({ limit: "50mb" }));
 app.use(express.urlencoded({ limit: "50mb", extended: true }));
@@ -23,7 +25,15 @@ app.use(cors({
 app.use('/api/auth' , authRouters) ; 
 app.use('/api/messages' , messageRouters) ; 
 
-server.listen(port , (res , req) => {
+if(process.env.NODE_ENV === "production") {
+  app.use(express.static(path.join(__dirname , "../frontend/dist")))
+
+  app.get("*" , (req , res) => {
+    res.sendFile(path.join(__dirname , "../frontend" , "dist" , "index.html"))
+  })
+}
+
+server.listen(port , () => {
     console.log(`Connect http://localhost:${port}`) ; 
     connectDB() ; 
 }) ; 
